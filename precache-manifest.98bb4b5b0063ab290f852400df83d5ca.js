9e203154adba641da32277bbdbe1ddf3af7831e3
Ali <alikeramatdev@gmail.com>
2022-07-17 16:58:25 +0430

    Updates

---
+++ precache-manifest.98bb4b5b0063ab290f852400df83d5ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71138f95f1775f0ed2a6",
+    "url": "/My-Todos/static/css/main.90b591b7.chunk.css"
+  },
+  {
+    "revision": "71138f95f1775f0ed2a6",
+    "url": "/My-Todos/static/js/main.71138f95.chunk.js"
+  },
+  {
+    "revision": "24758a8b680d33a928be",
+    "url": "/My-Todos/static/js/1.24758a8b.chunk.js"
+  },
+  {
+    "revision": "de7c07827a2e9b13eddb",
+    "url": "/My-Todos/static/js/runtime~main.de7c0782.js"
+  },
+  {
+    "revision": "feba3519b47b0cca5de38000f4f20201",
+    "url": "/My-Todos/index.html"
+  }
+];
